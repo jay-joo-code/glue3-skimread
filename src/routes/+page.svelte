32c70c7764e042b756238b4e07f006f6dfe2201f
Jay Joo <jj534@cornell.edu>
2023-04-23 23:10:54 -0400

    add padding to sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,64 +70,68 @@
 
 <PageContainer title="Home" limitWidth={true} layout="aside-main">
 	<Aside>
-		<form on:submit={fetchBooks}>
-			<TextInput class="rounded-full" placeholder="Search for books" bind:value={query} />
-		</form>
-		<div class="mt-4">
-			{#if isLoadingBooks}
-				<div class="mt-4 flex justify-center">
-					<div class="radial-progress animate-spin" style="--value:40; --size: 1.5rem;" />
-				</div>
-			{:else}
-				<div class="">
-					{#each books as book}
-						<button
-							class="border-b border-base-content/10 p-4 text-left text-sm"
-							on:click={() => {
-								selectedBook = book;
-								fetchBookSummary(book);
-							}}>{book}</button
-						>
-					{/each}
-				</div>
-			{/if}
-		</div>
-	</Aside>
-	<Main>
-		{#if selectedBook}
-			<h2 class="text-2xl font-bold leading-tight">{selectedBook}</h2>
-			<div class="mt-6">
-				{#if isLoadingSummary}
-					<div class="flex justify-center">
+		<div class="p-2">
+			<form on:submit={fetchBooks}>
+				<TextInput class="rounded-full" placeholder="Search for books" bind:value={query} />
+			</form>
+			<div class="mt-4">
+				{#if isLoadingBooks}
+					<div class="mt-4 flex justify-center">
 						<div class="radial-progress animate-spin" style="--value:40; --size: 1.5rem;" />
 					</div>
 				{:else}
-					{#each chapters as chapter}
-						<div class="border-b border-base-content/10 py-3">
+					<div class="">
+						{#each books as book}
 							<button
-								class=" text-left"
+								class="border-b border-base-content/10 p-4 text-left text-sm"
 								on:click={() => {
-									if (!chapterToDetails[chapter]) {
-										fetchChapterDetails(chapter);
-									}
-								}}
+									selectedBook = book;
+									fetchBookSummary(book);
+								}}>{book}</button
 							>
-								<p>{chapter}</p>
-							</button>
-							{#if loadingChapterTargets?.includes(chapter)}
-								<div class="mt-2 flex justify-center">
-									<div class="radial-progress animate-spin" style="--value:40; --size: 1.5rem;" />
-								</div>
-							{/if}
-							{#if chapterToDetails[chapter]}
-								<p class="mt-3 whitespace-pre-wrap text-sm leading-normal text-base-content/90">
-									{chapterToDetails[chapter]}
-								</p>
-							{/if}
-						</div>
-					{/each}
+						{/each}
+					</div>
 				{/if}
 			</div>
-		{/if}
+		</div>
+	</Aside>
+	<Main>
+		<div class="p-2">
+			{#if selectedBook}
+				<h2 class="text-2xl font-bold leading-tight">{selectedBook}</h2>
+				<div class="mt-6">
+					{#if isLoadingSummary}
+						<div class="flex justify-center">
+							<div class="radial-progress animate-spin" style="--value:40; --size: 1.5rem;" />
+						</div>
+					{:else}
+						{#each chapters as chapter}
+							<div class="border-b border-base-content/10 py-3">
+								<button
+									class=" text-left"
+									on:click={() => {
+										if (!chapterToDetails[chapter]) {
+											fetchChapterDetails(chapter);
+										}
+									}}
+								>
+									<p>{chapter}</p>
+								</button>
+								{#if loadingChapterTargets?.includes(chapter)}
+									<div class="mt-2 flex justify-center">
+										<div class="radial-progress animate-spin" style="--value:40; --size: 1.5rem;" />
+									</div>
+								{/if}
+								{#if chapterToDetails[chapter]}
+									<p class="mt-3 whitespace-pre-wrap text-sm leading-normal text-base-content/90">
+										{chapterToDetails[chapter]}
+									</p>
+								{/if}
+							</div>
+						{/each}
+					{/if}
+				</div>
+			{/if}
+		</div>
 	</Main>
 </PageContainer>
